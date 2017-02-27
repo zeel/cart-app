7307ef109cd74015bd243e98c07241ae506d98eb
zeel <zeel.shah@sprinklr.com>
2017-02-28 04:22:12 +0530

    remove company name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,28 @@
- import React, {Component} from 'react';
-import Icon from '../Icon';
+ import React, {
+ 	Component
+ } from 'react';
+ import Icon from '../Icon';
 
-import grid from 'icons/grid.svg';
-import add from 'icons/add.svg';
-import hamburger from 'icons/hamburger.svg';
-import Constants from 'constants/constants';
+ import grid from 'icons/grid.svg';
+ import add from 'icons/add.svg';
+ import hamburger from 'icons/hamburger.svg';
+ import Constants from 'constants/constants';
 
-import './_header.css';
+ import './_header.css';
 
-class Header extends Component{
+ class Header extends Component {
 
-	render(){
-		const props = this.props;
+ 	render() {
+ 		const props = this.props;
 
-		return <div className="full-width clearfix header">
-			<div className="lfloat header-title">ROPOSO</div>	
+ 		return <div className="full-width clearfix header">
+			<div className="lfloat header-title">CART-APP</div>	
 			<div className="rfloat">
 				<Icon onClick={props.onAddStory} name={add} className="scp header-icon"/>
 				<Icon onClick={props.onChangeViewType.bind(null, Constants.LIST)} name={hamburger} className="scp header-icon"/>
 				<Icon onClick={props.onChangeViewType.bind(null, Constants.GRID)} name={grid} className="scp header-icon"/>
 			</div>
 		</div>
-	}
-}
-export default Header;
+ 	}
+ }
+ export default Header;
